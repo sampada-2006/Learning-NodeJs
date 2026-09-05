d37da8dada3123faf76dbae938002fc6fba3a466
sampada-2006 <sampadajadhav2006@gmail.com>
2026-09-05 12:18:20 +0530

    Lerned how to create modules,export and import them

---
+++ Learning Modules/app.js
@@ -0,0 +1,11 @@
+//server banane ka kaam
+
+const http=require('http');
+const requestHandler=require('./user');
+const server = http.createServer(requestHandler);
+
+
+const PORT=3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`)
+});
---
+++ Learning Modules/user.js
@@ -0,0 +1,61 @@
+
+const fs=require('fs');
+
+const userRequestHandler=(req,res)=>{
+  console.log(req.url);
+  console.log(req.method);
+ // console.log(req.headers);
+  if(req.url==='/'){
+      res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>First Node Server</title></head>');
+  res.write('<body><h1>Welcome to Home</h1></body>');
+   res.write('<body><h1>Enter your Details:</h1>')
+   res.write('<form action="/submit-details" method="POST">');
+   res.write('<input type="text" name="username" placedolder="Enter your name"><br>');    
+   res.write('<label for="male"> Male </label>');              
+   res.write('<input type="radio" id="male" name="gender" value="male"><br>');
+   res.write('<label for="female">Female</label>');
+   res.write('<input type="radio" id="female" name="gender" value="female"><br>');
+   res.write('<input type="submit" value="submit">');
+   res.write('</form>');
+   res.write('</body>');
+  res.write('</html>');
+  res.end();
+  }else if(req.url.toLowerCase()==="/submit-details" && req.method=="POST"){
+    const body =[];
+
+      req.on("data", (chunk) =>{
+             console.log(chunk);
+             body.push(chunk);
+      })
+      req.on('end',() => {
+        const fullBody=Buffer.concat(body).toString();
+        const params= new URLSearchParams(fullBody);
+        console.log(fullBody);
+        //const bodyObject={};
+        //for(const [key,value] of params.entries()){
+          //bodyObject[key]=value;
+        //}
+        const bodyObject= Object.fromEntries(params);
+        console.log(bodyObject);
+        fs.writeFileSync('user.txt',JSON.stringify(bodyObject));
+      })
+      
+      res.statusCode=302; //for redirection
+      res.setHeader('Location', '/');
+      res.end(); 
+  }else{
+     res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>First Node Server</title></head>');
+  res.write('<body><h1>404</h1></body>');
+  res.write('</html>');
+  res.end("Hello World");    /*Browser displays "Hello World"*/
+  }
+  
+  //process.exit(); //Stops the event loop and kills the server
+};
+
+module.exports =userRequestHandler;
+
